8885b211d2b6322bf515d17aa94006fe22d3105a
redcloud1999 <ccbtata3@gmail.com>
2022-10-13 17:11:11 -0300

    set up project initial organization and metadata
    - Add setup.py turn instalable
    - Add __main__ for executable
    - Add __init__ for package
    
    closes #8

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='dundie',
+    version='0.1.0',
+    description='reward Point System for Dunder Mifflin',
+    author='Otávio Trindade',
+    packages=find_packages(),
+)
